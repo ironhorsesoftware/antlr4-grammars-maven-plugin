3948ec54d2d6db31ed8ed60ad59d930d449b805c
Michael Pigott <rpimike1022@yahoo.com>
2024-11-23 14:34:42 -0500

    Initial Setup (#1)
    Ran the command:
    
    ```
    mvn archetype:generate  \
      -DgroupId=com.ironhorsesoftware.maven.antlr4  \
      -DartifactId=antlr4-grammars-maven-plugin  \
      -Dversion=0.0.1-SNAPSHOT  \
      -DarchetypeGroupId=org.apache.maven.archetypes \
      -DarchetypeArtifactId=maven-archetype-plugin \
      -DinteractiveMode=false
    ```
    
    and made some tweaks to support the intended Java version and Java MOJO
    class name.
    
    ---------
    
    Co-authored-by: Mike Pigott <mpigott@gmail.com>

---
+++ create.sh
@@ -0,0 +1,7 @@
+mvn archetype:generate  \
+  -DgroupId=com.ironhorsesoftware.maven.antlr4  \
+  -DartifactId=antlr4-grammars-maven-plugin  \
+  -Dversion=0.0.1-SNAPSHOT  \
+  -DarchetypeGroupId=org.apache.maven.archetypes \
+  -DarchetypeArtifactId=maven-archetype-plugin \
+  -DinteractiveMode=false
